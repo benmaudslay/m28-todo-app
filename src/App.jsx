0e1a6e6fe39e531d0e91762950eac3e98a265345
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1235.local>
2021-09-27 11:24:00 +0100

    adding todos and displaying them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+/** AIMS
+ * Add todo items to a todo list
+ * Display todo list
+ * Mark completed items
+ * Delete items
+ **/
+
+/** COMPONENTS
+ * Input Form for the todos
+ * List component to show todos
+ * List item component for each todo
+ */
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleForm = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
+      <Form handleForm={handleForm} />
+      <ul>
+        {todos.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default App;
+
+// const func = (param) => {
+
+// }
+
+// func()
---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const Form = (props) => {
+    const [userInput, setUserInput] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        console.log("The form was submitted")
+        props.handleForm(userInput)
+        setUserInput("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+        </form>
+    )
+}
+
+export default Form
